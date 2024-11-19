b208a6b5a1eb417de9ae918faf6ffb692f525599
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-11-19 13:26:30 +0200

    adds api to create a province

---
+++ backend/src/main/java/com/tadiwa/backend/features/provinces/ProvinceController.java
@@ -0,0 +1,29 @@
+package com.tadiwa.backend.features.provinces;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tadiwa.backend.features.provinces.dto.AddProvinceDTO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("provinces")
+public class ProvinceController {
+
+    @Autowired
+    private ProvinceService provService;
+    
+    @PostMapping("/add")
+    public ResponseEntity<Province> addProvince(@RequestBody AddProvinceDTO dto) {
+        Province province = provService.createProvince(dto);
+
+        return ResponseEntity.ok(province);
+    }
+    
+
+}
