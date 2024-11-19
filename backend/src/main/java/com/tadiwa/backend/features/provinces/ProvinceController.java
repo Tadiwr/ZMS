4aef0bad76c163ff97449a79e3f5ae62d5b24ed3
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-11-19 13:37:59 +0200

    adds route to delete province

--- backend/src/main/java/com/tadiwa/backend/features/provinces/ProvinceController.java
+++ backend/src/main/java/com/tadiwa/backend/features/provinces/ProvinceController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -48,6 +49,17 @@ public class ProvinceController {
         return ResponseEntity.ok(province);
     }
     
-    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteById(@PathVariable Long id) {
+
+        try {
+            provService.deleteProvince(id);
+            return ResponseEntity.ok().build();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 
 }
