8f8cdddc7f364641b7e924b5cbbc395ac48c0c70
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-11-19 13:32:53 +0200

    adds api to get a province by its id

--- backend/src/main/java/com/tadiwa/backend/features/provinces/ProvinceController.java
+++ backend/src/main/java/com/tadiwa/backend/features/provinces/ProvinceController.java
@@ -6,12 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tadiwa.backend.features.provinces.dto.AddProvinceDTO;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("provinces")
@@ -31,6 +34,20 @@ public class ProvinceController {
     public List<Province> allProvinces() {
         return provService.getAllProvinces();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Province> getById(@PathVariable Long id) {
+        Optional<Province> provOptional = provService.getProvinceById(id);
+
+        if (provOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Province province = provOptional.get();
+
+        return ResponseEntity.ok(province);
+    }
+    
     
 
 }
