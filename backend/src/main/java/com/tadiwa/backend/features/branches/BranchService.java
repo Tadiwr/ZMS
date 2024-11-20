84edd61d6353039547cd5cc82f381266497b2051
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-11-20 15:19:35 +0200

    adds read, delete and update functions to branch service

--- backend/src/main/java/com/tadiwa/backend/features/branches/BranchService.java
+++ backend/src/main/java/com/tadiwa/backend/features/branches/BranchService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tadiwa.backend.features.branches.dto.AddBranchDTO;
+import com.tadiwa.backend.features.branches.dto.UpdateBranchDTO;
 import com.tadiwa.backend.features.partydistricts.PartyDistrict;
 import com.tadiwa.backend.features.partydistricts.PartyDistrictService;
 import com.tadiwa.backend.shared.exceptions.NotFound;
@@ -44,21 +45,25 @@ public class BranchService {
         return branchRepo.findAll();
     }
 
-    public Branch updateBrach(AddBranchDTO dto) throws NotFound {
-        Branch branch = new Branch();
-
+    public Branch updateBrach(UpdateBranchDTO dto) throws NotFound {
+        Optional<Branch> branchOption = branchRepo.findById(dto.getId());
         Optional<PartyDistrict> partyOptional = partyDistService.getPartyDistrictById(dto.getPartyDistrictId());
 
-        if (partyOptional.isEmpty()) {
+        if (partyOptional.isEmpty() || branchOption.isEmpty()) {
             throw new NotFound();
         }
 
         PartyDistrict partyDistrict = partyOptional.get();
+        Branch branch = branchOption.get();
 
         branch.setName(dto.getName());
         branch.setPartyDistrict(partyDistrict);
         
         return branchRepo.save(branch);
+    }
+
+    public void deleteBranch(Long id) {
+        branchRepo.deleteById(id);
     }
 
 }
