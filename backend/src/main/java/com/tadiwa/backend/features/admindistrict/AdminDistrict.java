e513caac2c06396ed4498b692dfc35d19d9eaa94
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-11-20 11:20:29 +0200

    adds admin districts

---
+++ backend/src/main/java/com/tadiwa/backend/features/admindistrict/AdminDistrict.java
@@ -0,0 +1,31 @@
+package com.tadiwa.backend.features.admindistrict;
+
+import com.tadiwa.backend.features.provinces.Province;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "admindistricts")
+public class AdminDistrict {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "province_id")
+    private Province province;
+    
+
+}
--- backend/src/main/java/com/tadiwa/backend/features/provinces/Province.java
+++ backend/src/main/java/com/tadiwa/backend/features/provinces/Province.java
@@ -1,10 +1,16 @@
 package com.tadiwa.backend.features.provinces;
 
 import jakarta.persistence.Table;
+
+import java.util.List;
+
+import com.tadiwa.backend.features.admindistrict.AdminDistrict;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Data
@@ -18,5 +24,8 @@ public class Province {
 
     @Column(nullable = false)
     private String name;
+
+    @OneToMany(mappedBy = "province")
+    private List<AdminDistrict> adminDistricts;
 
 }
