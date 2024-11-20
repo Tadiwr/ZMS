8296e304de57a25c9da92906807e61273f2527ed
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-11-20 19:56:33 +0200

    made DTO util class

---
+++ backend/src/main/java/com/tadiwa/backend/shared/tranferable/DtoUtil.java
@@ -0,0 +1,29 @@
+package com.tadiwa.backend.shared.tranferable;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+public class DtoUtil {
+    
+    /** Takes a `Transferable` and returns its Data Transfer Object form  */
+    public static <T> T transform(Transferable<T> transferable) {
+        return transferable.toDTO();
+    }
+
+    /** Takes a List of `Transferable` and returns its Data Transfer Object form  */
+    public static <T> List<T> transform(List<Transferable<T>> transferableList) {
+        return transferableList.stream()
+            .map(obj -> obj.toDTO())
+            .collect(Collectors.toList())
+        ;
+    }
+
+    /** Takes an Iterable of `Transferable` and returns its Data Transfer Object form  */
+    public static <T> List<T> transform(Iterable<Transferable<T>> transferableIterable) {
+        return StreamSupport.stream(transferableIterable.spliterator(), false)
+            .map(transferable -> transferable.toDTO())
+            .collect(Collectors.toList());
+    }
+
+}
