cda7518db9927bb1b904820959535b0385e7729f
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-11-24 20:54:40 +0200

    adds user details service

--- src/main/java/com/tadiwa/backend/features/user/User.java
+++ src/main/java/com/tadiwa/backend/features/user/User.java
@@ -1,5 +1,12 @@
 package com.tadiwa.backend.features.user;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,7 +17,7 @@ import lombok.Data;
 @Table(name = "users")
 @Entity
 @Data
-public class User {
+public class User implements UserDetails {
     
     @Id
     @GeneratedValue
@@ -19,5 +26,13 @@ public class User {
     @Column(unique = true)
     private String email;
     private String password;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>();
+    }
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
 
 }
