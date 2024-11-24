35c0b6bb89851fe8ccacb90f58ad9361ef3ee533
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-11-24 19:12:32 +0200

    made user column unique

--- src/main/java/com/tadiwa/backend/features/user/User.java
+++ src/main/java/com/tadiwa/backend/features/user/User.java
@@ -1,5 +1,6 @@
 package com.tadiwa.backend.features.user;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -15,6 +16,7 @@ public class User {
     @GeneratedValue
     private Long id;
 
+    @Column(unique = true)
     private String email;
     private String password;
 
