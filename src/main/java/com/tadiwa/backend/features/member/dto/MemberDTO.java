9cf8fabf4f5732621ef2ca757e67c64c3b162343
Tadiwanashe Shangwa <tshxng@gmail.com>
2024-11-26 18:37:06 +0200

    adds initializer on Member DTO

--- src/main/java/com/tadiwa/backend/features/member/dto/MemberDTO.java
+++ src/main/java/com/tadiwa/backend/features/member/dto/MemberDTO.java
@@ -3,6 +3,8 @@ package com.tadiwa.backend.features.member.dto;
 import java.time.LocalDateTime;
 import java.util.Date;
 
+import com.tadiwa.backend.features.member.Member;
+
 public record MemberDTO(
     Long id,
     String fullName,
@@ -15,5 +17,26 @@ public record MemberDTO(
     LocalDateTime createdAt,
     Long cellID
 ) {
+
+    public static MemberDTO from(Member member) {
+
+        if (member == null) {
+            return null;
+        }
+            
+        return new MemberDTO(
+            member.getId(),
+            member.getFullName(),
+            member.getDateOfBirth(),
+            member.getVoterRegNumber(),
+            member.getIdNumber(),
+            member.getPhysicalAddress(),
+            member.getPhoneNumber(),
+            member.getMembershipNumber(),
+            member.getCreatedAt(),
+            member.getCell().getId()
+        );
+
+    }
     
 }
